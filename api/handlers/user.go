6191642beafb40fbaed98df5874ee08f793940cd
Vishwajeet <vishwajeet@mabel.jewelry>
2022-12-10 00:26:10 +0530

    fixed download api

--- api/handlers/user.go
+++ api/handlers/user.go
@@ -10,12 +10,14 @@ import (
 
 func SignUp(c *gin.Context) {
 	res, err := user.SignUp(c)
+	fmt.Printf("Err %v\n", err)
 	statusCode, data := utils.FormatResponseMessage(res, err, http.StatusOK)
 	c.JSON(statusCode, data)
 }
 
 func SignIn(c *gin.Context) {
 	res, err := user.SignInUser(c)
+	fmt.Printf("Err %v\n", err)
 	statusCode, data := utils.FormatResponseMessage(res, err, http.StatusOK)
 	c.JSON(statusCode, data)
 }
@@ -30,7 +32,6 @@ func AddBasicInfo(c *gin.Context) {
 func GetBasicInfo(c *gin.Context) {
 	res, err := user.GetBasicInfoByUserId(c)
 	fmt.Printf("Err %v\n", err)
-
 	statusCode, data := utils.FormatResponseMessage(res, err, http.StatusOK)
 	c.JSON(statusCode, data)
 }
@@ -38,7 +39,6 @@ func GetBasicInfo(c *gin.Context) {
 func UpdateBasicInfo(c *gin.Context) {
 	res, err := user.UpdateBasicInfo(c)
 	fmt.Printf("Err %v\n", err)
-
 	statusCode, data := utils.FormatResponseMessage(res, err, http.StatusOK)
 	c.JSON(statusCode, data)
 }
@@ -52,18 +52,21 @@ func AddMeasurement(c *gin.Context) {
 
 func GetMeasurementByType(c *gin.Context) {
 	res, err := user.GetMeasurement(c)
+	fmt.Printf("Err %v\n", err)
 	statusCode, data := utils.FormatResponseMessage(res, err, http.StatusOK)
 	c.JSON(statusCode, data)
 }
 
 func GetAllMeasurementByDate(c *gin.Context) {
 	res, err := user.GetAllMeasurement(c)
+	fmt.Printf("Err %v\n", err)
 	statusCode, data := utils.FormatResponseMessage(res, err, http.StatusOK)
 	c.JSON(statusCode, data)
 }
 
 func GetAllMeasurementOfDate(c *gin.Context) {
 	res, err := user.GetAllMeasurementByDate(c)
+	fmt.Printf("Err %v\n", err)
 	statusCode, data := utils.FormatResponseMessage(res, err, http.StatusOK)
 	c.JSON(statusCode, data)
 }
@@ -77,69 +80,66 @@ func UploadFile(c *gin.Context) {
 
 func DownloadFile(c *gin.Context) {
 	file, name, _, err := user.DownloadFile(c)
+	fmt.Printf("\n%v\n", err)
 	if err != nil {
 		fmt.Printf("\n%v\n", err)
 		c.AbortWithStatusJSON(404, "Something went wrong")
 	}
 	c.FileAttachment(file, name)
-
-	//TODO create a cron job or clean api
-	/*err = utils.DeleteFiles(encFile)
-	if err != nil {
-		c.AbortWithStatusJSON(404,"Something went wrong")
-	}
-	err = utils.DeleteFiles(file)
-	if err != nil {
-		c.AbortWithStatusJSON(404,"Something went wrong")
-	}*/
-	//c.JSON(statusCode, data)
+	c.JSON(200, "ok")
 }
 
 func AddVisit(c *gin.Context) {
 	res, err := user.AddVisit(c)
 	fmt.Printf("Err %v\n", err)
-
 	statusCode, data := utils.FormatResponseMessage(res, err, http.StatusOK)
 	c.JSON(statusCode, data)
 }
 
 func GetAllVisit(c *gin.Context) {
 	res, err := user.GetAllVisits(c)
+	fmt.Printf("Err %v\n", err)
 	statusCode, data := utils.FormatResponseMessage(res, err, http.StatusOK)
 	c.JSON(statusCode, data)
 }
 
 func ShareHealthCard(c *gin.Context) {
 	res, err := user.ShareHealthCard(c)
+	fmt.Printf("Err %v\n", err)
 	statusCode, data := utils.FormatResponseMessage(res, err, http.StatusOK)
 	c.JSON(statusCode, data)
 }
 
 func GetAllHealthInsurance(c *gin.Context) {
 	res, err := user.GetAllHealthInsurance(c)
+	fmt.Printf("Err %v\n", err)
 	statusCode, data := utils.FormatResponseMessage(res, err, http.StatusOK)
 	c.JSON(statusCode, data)
 }
 
 func AddDonation(c *gin.Context) {
 	res, err := user.AddDonation(c)
+	fmt.Printf("Err %v\n", err)
 	statusCode, data := utils.FormatResponseMessage(res, err, http.StatusOK)
 	c.JSON(statusCode, data)
 }
 
 func GetAllDonations(c *gin.Context) {
 	res, err := user.GetAllDonations(c)
+	fmt.Printf("Err %v\n", err)
 	statusCode, data := utils.FormatResponseMessage(res, err, http.StatusOK)
 	c.JSON(statusCode, data)
 }
 
 func GetAllDonationsByUser(c *gin.Context) {
 	res, err := user.GetAllDonationsByUser(c)
+	fmt.Printf("Err %v\n", err)
 	statusCode, data := utils.FormatResponseMessage(res, err, http.StatusOK)
 	c.JSON(statusCode, data)
 }
 func ShareVerify(c *gin.Context) {
 	res, err := user.ShareVerify(c)
+	fmt.Printf("Err %v\n", err)
 	statusCode, data := utils.FormatResponseMessage(res, err, http.StatusOK)
 	c.JSON(statusCode, data)
 }
